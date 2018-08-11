a78d9f730a5a9f425a117cce0326db8af7a2ef7f
Karel Tuček <kareltucek@centrum.cz>
2018-08-11 10:39:29 +0200

    write some unit tests for the expression string handling

--- app/src/test/java/cz/ktweb/randomlistgenerator/ExampleUnitTest.java
+++ app/src/test/java/cz/ktweb/randomlistgenerator/ExampleUnitTest.java
@@ -10,8 +10,22 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void tokenizer_test() {
+        assertEquals( new String[]{"a","b","c","(","d","|","e",")","[5]"}, ExpressionEvaluator.tokenize("a, b, c( d|e)[5]"));
+        assertEquals( new String[]{"-5","-","5", "\"conc\"", "5","-","-5"}, ExpressionEvaluator.tokenize("-5-5 \"conc\" 5--5"));
     }
+
+    @Test
+    public void serializer_test() {
+        String test1 = "abc def_gah \"test\" test__ abc";
+        String test2 = "\"test\"";
+        String test3 = Utils.serializeString(test1);
+        assertEquals( test1, Utils.deserializeString(Utils.serializeString(test1)));
+        assertEquals( test2, Utils.deserializeString(Utils.serializeString(test2)));
+        assertEquals( test3, Utils.deserializeString(Utils.serializeString(test3)));
+    }
+
 }
